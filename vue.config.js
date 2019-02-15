ce7b53c633e9408786e602ce535f7c4ca87b9c8a
chris@t910 <chris@t910>
2019-02-15 14:35:41 +0100

    adjusted to orcl

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     devServer: {
-        proxy: "http://192.168.178.49:3344",
+        proxy: "http://chris.de.oracle.com:3344",
         host: '0.0.0.0',
         disableHostCheck: true
     }
